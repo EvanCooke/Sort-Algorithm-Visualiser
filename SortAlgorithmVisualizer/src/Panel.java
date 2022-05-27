1c37d0ee55431dc9b017441e32dc6fb53366ff09
Evan Cooke <evncooke@gmail.com>
2022-05-27 00:17:23 -0500

    Fixed Graphics to Display array

--- SortAlgorithmVisualizer/src/Panel.java
+++ SortAlgorithmVisualizer/src/Panel.java
@@ -2,25 +2,40 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Panel extends JPanel {
-    public static final int SCREEN_WIDTH = 1000;
-    public static final int SCREEN_HEIGHT = 600;
+    static final int SCREEN_WIDTH = 1000;
+    static final int SCREEN_HEIGHT = 600;
 
-    public int[] array = new int[100];
+    final int[] array = new int[100];
 
-    public static final int UNIT_SIZE = SCREEN_HEIGHT / 100; // UNIT_SIZE = SCREEN_HEIGHT / array.length
+    static final int UNIT_SIZE = SCREEN_WIDTH / 100; // UNIT_SIZE = SCREEN_HEIGHT / array.length
 
 
     Panel(){
         this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
         this.setBackground(Color.BLACK);
+        start();
+    }
+
+    public void start(){
+        for(int i = 0; i < array.length; i++){
+            array[i] = i;
+        }
     }
 
     // we do not need to invoke the paint method because it is called
     // automatically when we instantiate a component such as a JFrame
-//    @Override
-//    public void paintComponent(Graphics g) {
-//        for (int x = 0; x < SCREEN_WIDTH / UNIT_SIZE; x++) {
-//            //g.drawRect(x, (array[x] - SCREEN_HEIGHT), x, SCREEN_HEIGHT);
-//        }
-//    }
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        draw(g);
+    }
+
+    public void draw(Graphics g) {
+        for (int x = 0; x < SCREEN_WIDTH / UNIT_SIZE; x++) {
+            g.setColor(Color.LIGHT_GRAY);
+            g.fillRect(x * UNIT_SIZE, SCREEN_HEIGHT - array[x], UNIT_SIZE, array[x]);
+        }
+    }
+
+
 }
