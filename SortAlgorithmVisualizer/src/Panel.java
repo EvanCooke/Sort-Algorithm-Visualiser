d139a6c327c19d2873b033225508326a8a39229f
Evan Cooke <evncooke@gmail.com>
2022-05-27 16:30:07 -0500

    added timer

--- SortAlgorithmVisualizer/src/Panel.java
+++ SortAlgorithmVisualizer/src/Panel.java
@@ -1,20 +1,25 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Random;
 
-public class Panel extends JPanel {
+public class Panel extends JPanel implements ActionListener {
     static final int SCREEN_WIDTH = 1000;
     static final int SCREEN_HEIGHT = 600;
+    static final int DELAY = 500;
 
     final int[] array = new int[100];
+    Timer timer;
 
     static final int UNIT_SIZE_X = SCREEN_WIDTH / 100; // UNIT_SIZE = SCREEN_HEIGHT / array.length
     static final int UNIT_SIZE_Y = SCREEN_HEIGHT / 100;
 
-
     Panel(){
         this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
         this.setBackground(Color.BLACK);
+        timer = new Timer(DELAY, this);
+        timer.start();
         start();
     }
 
@@ -40,9 +45,11 @@ public class Panel extends JPanel {
             array[randomIndex] = temp;
             lastIndex--;
         }
+
+        //bubbleSort(array);
     }
 
-    public int[] bubbleSort(int[] inputArray){
+    public void bubbleSort(int[] inputArray){
         boolean swapped;
         do {
             swapped = false;
@@ -53,16 +60,18 @@ public class Panel extends JPanel {
                     array[i] = array[i + 1];
                     array[i + 1] = temp;
                 }
+                repaint();
+                
             }
         } while (swapped);
-        return inputArray;
+        //return inputArray;
     }
 
     // we do not need to invoke the paint method because it is called
     // automatically when we instantiate a component such as a JFrame
     @Override
     public void paintComponent(Graphics g) {
-        super.paintComponent(g);
+        super.paintComponent(g); // paints background?
         draw(g);
     }
 
@@ -73,5 +82,24 @@ public class Panel extends JPanel {
             // update this to allow for any number of elements in array to be displayed evenly
             g.fillRect((x * UNIT_SIZE_X) + 1, (SCREEN_HEIGHT - (array[x] * UNIT_SIZE_Y)), UNIT_SIZE_X - 1, array[x] * UNIT_SIZE_Y);
         }
+    }
+
+    // executed after DELAY has passed for timer
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        boolean swapped;
+        do {
+            swapped = false;
+            for (int i = 0; i < array.length - 1; i++) {
+                if (array[i] > array[i + 1]) {
+                    swapped = true;
+                    int temp = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = temp;
+                }
+                repaint();
+            }
+        } while (swapped);
+        //return inputArray;
     }
 }
